3b099a878fdd3cb76c334cfe83ddd8e48c69bdd7
David Lechner <david@lechnology.com>
2020-01-10 12:12:54 -0600

    ev3dev-bionic

--- debian/release.sh
+++ debian/release.sh
@@ -6,16 +6,11 @@ set -e
 
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
-distribution=$(dpkg-parsechangelog -S Distribution)
-codename=$(debian-distro-info --codename --${distribution})
 
-OS=debian DIST=${codename} ARCH=amd64 pbuilder-ev3dev build
-OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
+debuild -S
 
-debsign ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
-debsign ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
+dput ppa:ev3dev/tools ../${source}_${version}_source.changes
 
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
-dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
+debuild -- clean
 
 gbp buildpackage --git-tag-only
